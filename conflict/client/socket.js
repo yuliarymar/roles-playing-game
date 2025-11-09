23c04ee520dbafc8ae9bc72251953c547fee8d56
yuliarymar <yliarymar261@gmail.com>
2025-11-10 00:12:05 +0200

    ппс

--- conflict/client/socket.js
+++ conflict/client/socket.js
@@ -1,8 +1,14 @@
-// client/src/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('https://roles-playing-game.onrender.com', {
+// Автоматичний вибір URL
+const BACKEND_URL = import.meta.env.DEV 
+  ? 'http://localhost:3001'
+  : 'https://roles-playing-game.onrender.com';
+
+const socket = io(BACKEND_URL, {
   autoConnect: true,
+  secure: true,  // ← ОБОВ'ЯЗКОВО ДЛЯ МОБІЛЬНИХ!
+  transports: ['websocket', 'polling'],  // ← Підтримка старих браузерів
   reconnection: true,
   reconnectionDelay: 1000,
   reconnectionDelayMax: 5000,
