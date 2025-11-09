16136fa6c2120549295f12ccf0f50a163999c36a
yuliarymar <yliarymar261@gmail.com>
2025-11-09 22:38:53 +0200

    : correct backend URL (Render)"

--- conflict/client/socket.js
+++ conflict/client/socket.js
@@ -1,7 +1,7 @@
 // client/src/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001', {
+const socket = io('https://roles-playing-game.onrender.com', {
   autoConnect: true,
   reconnection: true,
   reconnectionDelay: 1000,
